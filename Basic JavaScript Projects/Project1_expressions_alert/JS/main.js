461c8a267ca1e1456e7d0f6b4d81e92db8869db2
Mawci <79284679+Mawci@users.noreply.github.com>
2024-08-16 10:08:47 -0500

    JavaScript Assignment Submission

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+var alertMessage = "HELLO WORLD!!", Sent1 = " I said, ", Sent2 = "\"Don't go to that party\""; //declaring and assigning 3 variables to hold string values with one utilizing the escape character
+
+window.alert(alertMessage);//calling the widow.alert function and passsing the variable alertMessage that holds a string
+var num = 3 + 3;//declaring and assigning a variable num that holds the result of an expression 
+document.write(num + " ");//calling the document.write function to display to the value the variable is holding from the expression
+
+document.write(Sent1 + Sent2);//calling the document.write fucntion to display and example of combing 2 strings
